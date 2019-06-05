c0bafef2979681bafcde10aabc447c63d64aa386
htoth <31545229+htoth18@users.noreply.github.com>
2019-06-04 20:21:40 -0400

    back end fix for registry links

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -14,4 +14,15 @@ jQuery (document).ready(function(){
         });
         return false;
     });
+    
+    $('a').each(function() {
+        var a = new RegExp('/' + window.location.host + '/');
+        if(!a.test(this.href)) {
+            $(this).click(function(event) {
+               event.preventDefault();
+               event.stopPropagation();
+               window.open(this.href, '_blank');
+            });
+        }
+    });
 });
